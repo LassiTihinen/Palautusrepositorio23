5f5cd55a7005a5fc0e5d62149c588ab6db280193
LassiTihinen <tihinenlassi@gmail.com>
2023-09-24 18:34:38 +0300

    viikko 4 palautus

---
+++ viikko4/SecondScreen.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { StyleSheet, View, Text, BackHandler} from "react-native";
+
+export default function SecondScreen({route,navigation}){
+    useEffect(() => {
+        if(route.params?.message){
+            alert(route.params.message);
+        }
+        BackHandler.addEventListener('hardwareBackPress',close);
+        return() =>{
+            BackHandler.removeEventListener('hardwareBackPress',close);
+        }
+    }, [])
+    function close(){
+        navigation.goBack(null);
+        return true;
+    }
+
+
+    return(
+        <View styles={styles.container}>
+            <Text>SecondScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding:10,
+    }
+});
